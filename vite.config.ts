9bbfc2ac4ccaae92a35c11349e25f45c98ebc4b8
lizaansver <lizaansver@gmail.com>
2025-08-08 18:48:25 +0300

    Fix buildl

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/test/', // Убедись, что это имя твоего репозитория
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
